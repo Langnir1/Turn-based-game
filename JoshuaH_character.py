1b8358e1f7f578f25b91985867d65ab782ce7feb
Langnir1 <joshua.halamka@bsu.edu>
2024-10-18 19:46:21 -0400

    New update
    update

--- JoshuaH_character.py
+++ JoshuaH_character.py
@@ -1,8 +1,9 @@
-#This is a change
-
+import random
 
 class Character(object):
-    
+  # Check if my stuff is right
+  # What do I do about the tbc module
+  # Did I implement the 
     def __init__(self, name = "Velder",
                  hitPoints = 20,
                  hitChance = 70,
@@ -21,7 +22,6 @@ class Character(object):
     
     @name.setter
     def name(self, value):
-        value = testInt(self, value)
         self.__name = value
     
     @property
@@ -30,41 +30,87 @@ class Character(object):
     
     @hitPoints.setter
     def hitPoints(self, value):
-        self.hitPoints = value
+        self.__hitPoints = self.testInt(value, 0, 100, 10)
+        
+    @property
+    def hitChance(self):
+        return self.__hitChance
+    
+    @hitChance.setter
+    def hitChance(self, value):
+        self.__hitChance = self.testInt(value, 0, 100, 50)
        
     @property
     def maxDamage(self):
         return self.__maxDamage
     
+    @maxDamage.setter
+    def maxDamage(self, value):
+        self.__maxDamage = self.testInt(value, 0, 100, 10)
+    
     @property
-    def armor():
+    def armor(self):
         return self.__armor
     
+    @armor.setter
+    def armor(self, value):
+        self.__armor = self.testInt(value, 0, 10, 2)
+        
     def printStats(self):
-        print(name)
-        print(hitPoints)
-        print(maxDamage)
-        print(armor)
+        print(self.name)
+        print(self.hitPoints)
+        print(self.hitChance)
+        print(self.maxDamage)
+        print(self.armor)
     
-    #def hit(self, enemy):
+    def hit(self, enemy):
+        if random.randint(1, 100) < self.hitChance:
+            print(f"{self.name} hits {enemy.name}.")
+            damage = random.randint(1, self.maxDamage)
+            print(f"{enemy.name} takes {damage} points of damage")
+            damage -= enemy.armor
+            if damage < 0:
+                damage = 0
+            if enemy.armor > 0:
+                print(f"{enemy.name} has armor and it takes {enemy.armor} points of damage")
+            enemy.hitPoints -= damage
+            return enemy.hitPoints
+        else:
+            print(f"{self.name} missed {enemy.name}")
+            
         
     def testInt(self, value, min = 0, max = 100, default = 0):
-    """ takes in value 
+        """takes in value 
         checks to see if it is an int between
         min and max.  If it is not a legal value
         set it to default """
 
-    out = default
+        out = default
 
-    if type(value) == int:
-        if value >= min:
-            if value <= max:
-                out = value 
+        if type(value) == int:
+            if value >= min:
+                if value <= max:
+                    out = value 
+                else:
+                    print("Too large")
             else:
-                print("Too large")
+                print("Too small")
         else:
-            print("Too small")
-    else:
-        print("Must be an int")
+            print("Must be an int")
 
-    return out
+        return out
+    
+    def fight(hero, monster):
+        
+
+def main():
+    hero = Character()
+    hero.printStats()
+    enemy = Character("Goblin", 20, 30, 5, 5)
+    enemy.printStats()
+    hero.hit(enemy)
+    enemy.printStats()
+    
+
+if __name__ == "__main__":
+    main()
